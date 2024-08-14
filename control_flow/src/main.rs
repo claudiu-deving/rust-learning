b7a92b04c0182b65de038d979984db7647acff74
claudiu <=>
2024-08-14 21:21:29 +0300

    Fixes

--- control_flow/src/main.rs
+++ control_flow/src/main.rs
@@ -1,8 +1,30 @@
 use std::io;
 mod fibonacci;
+mod temperatur_conversion;
 
 fn main() {
-    let mut input=String::new();
+    loop{
+
+        println!("What do you want to test?");
+        println!("Press 1 for retrieving the nth fibonacci number");
+        println!("Press 2 to convert from Celsius to Fahrenheit and vice-versa");
+        println!("Write quit to exit.");
+       let mut initial_input=String::new();
+       io::stdin()
+           .read_line(&mut initial_input)
+           .expect("Invalid Input");
+        if initial_input.trim() == "1" {
+           fibonacci();
+        }else if initial_input.trim()=="2" {
+           temperatur_conversion::temperature_conversion();
+        }else if initial_input.trim()=="quit" {
+           break;
+        }
+    }
+}
+
+fn fibonacci(){
+ let mut input=String::new();
     println!("Enter the nth fibonacci number");
     io::stdin()
         .read_line(&mut input)
@@ -11,5 +33,5 @@ fn main() {
 
     let result =fibonacci::get_fibonacci_number(input);
     println!("The {0}th fibonacci number is: {1}",input,result);
-}
 
+}
---
+++ control_flow/src/temperatur_conversion.rs
@@ -0,0 +1,47 @@
+use std::io;
+pub fn temperature_conversion(){
+
+    loop{
+        println!("Type 1 for converting celcius into fahrenheit");
+        println!("Type 2 for converting fahrenheit into celcius");
+        println!("Type quit to exit");
+        let mut input_type = String::new();
+        io::stdin()
+            .read_line(&mut input_type)
+            .expect("Failed");        
+        match input_type.trim() {
+            "1"=>{
+               println!("Enter the temperature in celcius to be converted");
+               let mut temperature=String::new();
+               io::stdin()
+                    .read_line(&mut temperature)
+                    .expect("Failed");
+               let temperature:f32= temperature.trim().parse().expect("Please input a valid temperature");
+
+               println!("{0} degrees celcius is {1} degrees fahrenheit.",temperature,celcius_to_fahrenheit(temperature));
+               },
+            "2"=> {
+                println!("Enter the temperature in fahrenheit to be converted");
+                let mut temperature=String::new();
+                io::stdin()
+                     .read_line(&mut temperature)
+                     .expect("Failed");
+                let temperature:f32 = temperature.trim().parse().expect("Please input a valid temperature");
+
+                println!("{0} degrees fahrenheit is {1} degrees celcius ",temperature,fahrenheit_to_celcius(temperature));
+            },
+            "quit"=>{
+                break;
+            },
+            _=> println!("Enter a valid option")
+        }
+    }
+}
+
+fn fahrenheit_to_celcius(temperature_in_fahrenheit:f32)->f32{
+    (temperature_in_fahrenheit-32.0)/1.8
+}
+
+fn celcius_to_fahrenheit(temperature_in_celcius:f32)->f32{
+    (temperature_in_celcius*1.8)+32.0
+}
