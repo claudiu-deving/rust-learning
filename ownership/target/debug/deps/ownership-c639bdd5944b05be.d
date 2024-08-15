a7fcb98d5d5be5b87f6efc49855233f555f413bf
claudiu <=>
2024-08-15 22:44:11 +0300

    removed target files from tracking

--- ownership/target/debug/deps/ownership-c639bdd5944b05be.d
+++
@@ -1,5 +0,0 @@
-/home/clas/projects/ownership/target/debug/deps/ownership-c639bdd5944b05be: src/main.rs
-
-/home/clas/projects/ownership/target/debug/deps/ownership-c639bdd5944b05be.d: src/main.rs
-
-src/main.rs:
--- ownership/target/debug/ownership.d
+++
@@ -1,1 +0,0 @@
-/home/clas/projects/ownership/target/debug/ownership: /home/clas/projects/ownership/src/main.rs
