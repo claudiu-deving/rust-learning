345ded501a87c2457709a36ca2781c5d091ed2ff
claudiu <=>
2024-08-15 22:40:43 +0300

    Borrowing stuff

---
+++ ownership/target/debug/deps/ownership-c639bdd5944b05be.d
@@ -0,0 +1,5 @@
+/home/clas/projects/ownership/target/debug/deps/ownership-c639bdd5944b05be: src/main.rs
+
+/home/clas/projects/ownership/target/debug/deps/ownership-c639bdd5944b05be.d: src/main.rs
+
+src/main.rs:
---
+++ ownership/target/debug/ownership.d
@@ -0,0 +1,1 @@
+/home/clas/projects/ownership/target/debug/ownership: /home/clas/projects/ownership/src/main.rs
